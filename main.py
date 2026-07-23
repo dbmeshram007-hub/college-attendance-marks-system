c5b54518c2f3172e2a6868af85ae2bf8bb5faacf
Dhananjay <dbmeshram007@gmail.com>
2026-07-23 16:29:31 +0530

    Fixed CORS allow_credentials conflict

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="College Attendance & Marks API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"], 
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
