db9235bc38bae66c56e1ad962fe47f244e82f59e
Dhananjay <dbmeshram007@gmail.com>
2026-07-23 15:57:06 +0530

    Fixed database connection string to use Session mode

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlmodel import create_engine, Session
 
 # Make sure this has your correct password and port 5433
-DATABASE_URL="postgresql://postgres.rtjeaobxcwgjzeupzcni:Ayyappa%*563@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres"
+DATABASE_URL=DIRECT_URL="postgresql://postgres.rtjeaobxcwgjzeupzcni:Ayyappa%*563@aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres"
 
 engine = create_engine(DATABASE_URL, echo=True)
 def get_db():
